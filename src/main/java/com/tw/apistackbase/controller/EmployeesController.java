46d2204a667a575b7d756f755ec90cf715e4cd5e
FelicityAmiao <1760077814@qq.com>
2019-07-17 00:31:14 +0800

    feat: add to get all employees by GET  /employees

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeesController.java
@@ -0,0 +1,20 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesController {
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = Employee.createTestEmployees();
+        return employees;
+    }
+
+}
---
+++ src/test/java/com/tw/apistackbase/EmployeesControllerTest.java
@@ -0,0 +1,42 @@
+package com.tw.apistackbase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class EmployeesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_employees_when_call_get_all_employees() throws Exception {
+        mockMvc.perform(get("/employees"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json("[\n" +
+                        "    {\n" +
+                        "        \"id\": 4,\n" +
+                        "        \"name\": \"alibaba1\",\n" +
+                        "        \"age\": 20,\n" +
+                        "        \"gender\": \"male\",\n" +
+                        "        \"salary\": 6000\n" +
+                        "    }\n" +
+                        "]"));
+    }
+}
